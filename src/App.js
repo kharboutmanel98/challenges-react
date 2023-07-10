a55281716df46b2ebd171bb36e5ca4de0fdcac22
kharboutmanel98 <kharbout2017@gmail.com>
2023-07-10 15:46:28 +0200

    list challenge

--- src/App.js
+++ src/App.js
@@ -5,40 +5,44 @@ import Post from "./Post";
 import SideMenu from './SideMenu';
 
 function App() {
+
+
+  const posts = [
+    { id: 1 , postName: "Pingucoder" , postBody: "platform e learning"},
+    { id: 2 , postName: "Learn React" , postBody: "with pingucoder"},
+    { id: 3 , postName: "Learn ExpressJs" , postBody: "with pingucoder"},
+    { id: 4 , postName: "Learn Angular" , postBody: "with pingucoder"},
+    { id:'', postName:'', postBody:''},
+  ];
+  
+   
+  
   return (
     <div className="App">
-     <Header />
-  <div style={{display: "flex" , justifyContent: "center"}}>
+      <Header />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {/* POSTS & SIDE MENU CONTAINER */}
+        <div
+          style={{ display: "flex", width: "60%", justifyContent: "center" }}
+        >
+          {/* POSTS CONTAINER  */}
+          <div style={{ width: "70%" }}>
+            {posts.map((post, index) => (
+              <Post key={index} postName={post.postName} postBody={post.postBody} />
 
-   
+            ) )}
 
-     {/* POSTS & SIDE MENU CONTAINER */}
-    <div style={{display: "flex", width:"60%", justifyContent:"center"}}>
-
-
-         {/* POSTS CONTAINER  */}
-      <div style={{width: "70%" }}>
-            <Post postName= "Pingucoder" postBody="platform e learning"/>
-            <Post postName= "Learn React" postBody="with pingucoder"/>
-            <Post postName= "Learn ExpressJs" postBody="with pingucoder"/>
-            <Post postName= "Learn Angular" postBody="with pingucoder"/>
-            <Post />
-            
-            </div>
-         {/* POSTS CONTAINER  */}
-         {/* SIDE MENU CONTAINER */}
-         <div style={{width: "30%", marginTop:"25px"}}>
-          <SideMenu />
-         </div>
-         {/* SIDE MENU CONTAINER */}
+          </div>
+          {/* POSTS CONTAINER  */}
+          {/* SIDE MENU CONTAINER */}
+          <div style={{ width: "30%", marginTop: "25px" }}>
+            <SideMenu />
+          </div>
+          {/* SIDE MENU CONTAINER */}
+        </div>
+        {/* POSTS & SIDE MENU CONTAINER */}
       </div>
-      {/* POSTS & SIDE MENU CONTAINER */}
-
     </div>
-  </div>
-
-
-    
   );
 }
 
--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,6 @@
 import TagButton from "./TagButton"
 
+
 function Post({postName= "no Title", postBody="no body"}) {
   return (
     <div style={{padding: "10px", border: "solid #1d9bf0 5px" , margin: "25px"}}>
