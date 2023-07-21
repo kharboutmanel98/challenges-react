ab549af3ef85ab3927cb1b8ebfc7dd255872a66f
kharboutmanel98 <kharbout2017@gmail.com>
2023-07-21 13:41:42 +0200

    other form fields with state

--- src/Form.js
+++ src/Form.js
@@ -2,44 +2,85 @@ import { useState } from "react";
 
 
 function Form() {
-    // const [nameInput, setNameInput]=useState("");
-    // const [emailInput, setEmailInput]=useState("");
+   
   const [formInputs, setFormInputs]= useState({
     name: "",
     email: "",
-    age:""
+    age:"", 
+    generalInfo:"",
+    isStudent: false,
+    country:"",
+    status:""
     
   })
+
+  function handleCheckBoxChecked(event) {
+    setFormInputs({...formInputs, isStudent: event.target.checked})
+  }
+  
   return (
     <form onSubmit={(event)=>{
         event.preventDefault()
         console.log(formInputs)
     }}>
 
-        <label>name:</label>
+        <label>Name:</label>
         <input value={formInputs.name} onChange={(event) => {
-          // const newFormInput = {...formInputs};
-          // newFormInput.email = event.target.value
-          // setFormInputs(newFormInput)} 
           setFormInputs({...formInputs, name: event.target.value})
         } }
         />
         <hr />
         
 
-        <label>email:</label>
+        <label>Email:</label>
         <input value={formInputs.email} onChange={(event) => 
           setFormInputs({...formInputs, email: event.target.value})
           }
            />
         <hr />
 
-        <label>age:</label>
+        <label>Age:</label>
         <input value={formInputs.age} onChange={(event) =>
          setFormInputs({...formInputs, age: event.target.value})} />
         <hr />
-       
+        
+        <label>Are you student</label>
+        <input  type="checkbox"  checked={formInputs.isStudent} onChange={handleCheckBoxChecked}/>
+        <hr />
 
+          <div>
+          <input 
+                 value="student"
+                 type="radio" name="status" 
+                 checked={formInputs.status == "student"}
+                 onChange={(event) =>
+                 setFormInputs({...formInputs, status: event.target.value})}
+                 />
+          student
+          <input 
+                 value="teacher"
+                 type="radio" name="status" 
+                 checked={formInputs.status == "teacher"}
+                 onChange={(event) =>
+                 setFormInputs({...formInputs, status: event.target.value})}
+                 />
+          teacher
+          </div>
+          <hr />
+
+        <select value={formInputs.country} onChange={(event) =>
+         setFormInputs({...formInputs, country: event.target.value})} >
+          <option>Jordan</option>
+          <option>Marshall Islands</option>
+          <option>Kenya</option>
+        </select>
+        <hr />
+
+        <label>General Info</label>
+        <textarea value={formInputs.generalInfo} onChange={(event) => {
+           setFormInputs({...formInputs, generalInfo: event.target.value})
+        }} />
+        <hr />  
         <button>submit</button>
         
     </form>
