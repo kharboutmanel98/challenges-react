bdaf3241dec873bfda831ebaf7d3a54b6a865726
kharboutmanel98 <kharbout2017@gmail.com>
2023-07-28 18:46:22 +0200

    using array as state in the component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 
 import './App.css';
-import Scoreboard from "./Scoreboard"
+// import Scoreboard from "./Scoreboard"
 
-
+import { useState } from 'react';
 
 function App() {
 
+const [deviceNameInputValue, setDeviceNameInputValue] = useState("") 
+const [devices, setDevices] = useState(["iphone", "mac", "samsung", "windows"])
+const devicesList = devices.map((device) => {
+  return <li>{device}</li>
+});
+
+function handleAddClick (){
+
+  setDevices([...devices, deviceNameInputValue]);
+  // const newDevices = [...devices];
+  // newDevices.push(deviceNameInputValue)
+  // setDevices(newDevices)
+  
+}
  
   return (
     <div className="App" style={{
       paddingTop: "100px"
     }}>
-      <>
+     
       {/* <Form /> */}
-      <Scoreboard />
-      </>
+      {/* <Scoreboard /> */}
+      {devicesList}
+      <label>Device Name:</label>
+      <input type="text" value={deviceNameInputValue} onChange={(event) => {setDeviceNameInputValue(event.target.value)}}/>
+      <button onClick={handleAddClick}>Add</button>
+      
     </div>
   );
 }
